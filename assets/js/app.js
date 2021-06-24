2d2b78f7f1c555308bab27d028122b4711747a1c
MattBCoding <mattbodden@googlemail.com>
2021-06-24 22:30:40 +0000

    Add comments to app.js to layout functionality from notes

---
+++ assets/js/app.js
@@ -0,0 +1,56 @@
+// Globally needed variables
+// Capture player system colour scheme preference
+
+// FUNCTIONS
+
+// SETTINGS
+// Change Colour Scheme
+// Change Game Difficulty
+// Change Game Winning Condition
+
+// FUNCTIONS NEEDED BY MORE THAN ONE SCREEN
+// Set Game Mode
+// Determine Game Mode
+// Add Scoreboard to screen
+// Classic Game Logic
+// Spock Game Logic
+// Computer move selection - Math.random for easy difficulty
+// Computer move selection - hard difficulty
+// Player win round
+// Player lose round
+// Round is a draw
+// Player Win Game
+// Player lose game
+
+// SPOCK GAME SCREEN
+// Spock Game Event Capture
+// In game rules modal
+// Spock Rules modal open
+// Spock Rules modal close
+// Quit Game button - Spock
+
+// CLASSIC GAME SCREEN
+// Classic Game Event Capture
+// In game rules modal
+// Classic Rules modal open
+// Classic Rules modal close
+// Quit Game button - Classic
+
+// ENTER NAME SCREEN
+// Capture Name Entry by player
+// Change to Game Screen - Classic
+// Change to Game Screen - Spock
+// Back or Quit button
+
+// HOME SCREEN
+// Change to Name Screen
+// M.M. What happens when player clicks play classic mode
+// M.M. What happens when player clicks play spock mode
+// M.M. Open Game Settings Screen Modal
+// M.M. Close Game Settings Screen Modal
+// M.M. Open Rules Modal
+// M.M. Close Rules Modal
+// Get Main Menu Elements - getElementById
+// Main Menu Event Listeners - capture player click events
+// Main Menu Modal elements - getElementById
+// Main Menu Modal Event Listeners - capture player click events
